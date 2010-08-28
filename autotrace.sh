fb47a88a798f708076fd0a40093a49cadb975a7a
Tilman Blumenbach <tilman@ax86.net>
2010-08-28 18:48:13 +0200

    Forgot this is a Bazaar repository, duh. - Make sure the scripts are POSIX compliant. - Improve comments before release.

--- autotrace.sh
+++ autotrace.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 #       This script is an AutoTrace wrapper for FontForge: It calls autotrace
-#       with arguments that do not really produce nice results, but in essence
-#       only convert the BMP to EPS data.
+#       with arguments that do not produce nice results (e. g. a nice traced
+#       image), but instead basically only convert the BMP to EPS.
 #
 #       Verified to work with autotrace 0.31.1.
 #
-#       Copyright (c) 2009 by Tilman Blumenbach
+#       CALL FOR HELP: This is not an exact conversion! I didn't manage
+#                      to get ImageMagick's convert program to work with
+#                      FontForge because it produces too complex EPS, which
+#                      FontForge does not understand... Any ideas?
+#
+#       Copyright (c) 2009-2010 by Tilman Blumenbach <tilman@ax86.net>
 #       All rights reserved.
 #       
 #       Redistribution and use in source and binary forms, with or without
@@ -18,9 +23,9 @@
 #         copyright notice, this list of conditions and the following disclaimer
 #         in the documentation and/or other materials provided with the
 #         distribution.
-#       * Neither the name of the the author nor the names of its
-#         contributors may be used to endorse or promote products derived from
-#         this software without specific prior written permission.
+#       * Neither the name of the author nor the names of its contributors
+#         may be used to endorse or promote products derived from this
+#         software without specific prior written permission.
 #       
 #       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 #       "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -34,8 +39,8 @@
 #       (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 #       OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# Call autotrace with special arguments to get a result which resembles the original bitmap glyph and
-# cut out ugly border lines:
+# Call autotrace with special/bogus arguments to get a result which resembles
+# the original bitmap glyph and then remove ugly border lines using awk:
 autotrace -corner-threshold 360 -corner-always-threshold 360 "$@" | awk \
 	'BEGIN {
 		noprint = 0;
--- mkttf.sh
+++ mkttf.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #       This script uses fontforge and mkitalic to generate medium, bold and
-#       italic TTF versions of the terminus font from the BDF files.
+#       italic TTF versions of the terminus font from its BDF files.
 #
-#       Copyright (c) 2009-2010 by Tilman Blumenbach
+#       Copyright (c) 2009-2010 by Tilman Blumenbach <tilman@ax86.net>
 #       All rights reserved.
 #       
 #       Redistribution and use in source and binary forms, with or without
@@ -15,9 +15,9 @@
 #         copyright notice, this list of conditions and the following disclaimer
 #         in the documentation and/or other materials provided with the
 #         distribution.
-#       * Neither the name of the the author nor the names of its
-#         contributors may be used to endorse or promote products derived from
-#         this software without specific prior written permission.
+#       * Neither the name of the author nor the names of its contributors
+#         may be used to endorse or promote products derived from this
+#         software without specific prior written permission.
 #       
 #       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 #       "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -31,18 +31,42 @@
 #       (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 #       OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+### Helper functions ###
+
+# Display an error message and exit, using a custom return code.
+# Args   : $1   - The return code.
+#          $... - The error message to show.
+# Echoes : Nothing.
+# Returns: Nothing.
+error()
+{
+	ret=$1
+	shift
+
+	echo "Error: $*"
+	exit $ret
+}
+
 # Make a path absolute by prepending $PWD if necessary.
 # Args   : $1 - The path.
 # Echoes : The absolute path.
 # Returns: 0
 mkabs()
 {
-	if [ `expr substr "$1" 1 1` != / ]; then
-		echo "${PWD}/${1}"
-	else
-		echo "$1"
-	fi
+	case "$1" in
+		/*)
+			# Path is already absolute:
+			echo "$1"
+			;;
+
+		*)
+			# This appears to be a relative path, prepend the current
+			# working directory:
+			echo "${PWD}/${1}"
+			;;
+	esac
 }
+
 
 # The absolute path to this file's directory.
 MYDIR=`dirname "$0"`
@@ -58,19 +82,20 @@ fi
 
 mkitalic -h 1>/dev/null 2>&1
 if [ $? -eq 127 ]; then
-	echo 'mkitalic not found in your PATH.'
-	exit 2
+	# POSIX specifies a return code of 127 on a command search failure.
+	error 2 'mkitalic not found in your PATH.'
 fi
 
 # The path to the directory where the BDF files reside.
 TERMINUS_DIR=`mkabs "$1"`
 
 if [ ! -f "${TERMINUS_DIR}/ter-u12n.bdf" ]; then
-	echo 'The given directory does not look like a Terminus font source directory.'
-	exit 3
+	error 3 'The given directory does not look like a Terminus font source directory:' \
+		'It does not contain a file named "ter-u12n.bdf".'
 fi
 
-mkdir normal bold italic 1>/dev/null 2>&1
+# -p to suppress errors.
+mkdir -p normal bold italic || error 4 'Could not create target directories.'
 
 # Generate italic BDF files:
 echo 'Generating italic BDF files...'
@@ -79,15 +104,17 @@ for bdf in "$TERMINUS_DIR"/ter-u*n.bdf; do
 	mkitalic < "$bdf" > "${TERMINUS_DIR}/${BDF_BASENAME}i.bdf"
 done
 
-# Generate the TTF fonts:
+# Generate the TTF fonts.
+# This tells FontForge to use our AutoTrace wrapper.
 export AUTOTRACE="${MYDIR}/autotrace.sh"
 
 for weight in normal bold italic; do
 	echo "Generating ${weight} font..."
 
-	pushd "$weight" 1>/dev/null
-	"${MYDIR}/mkttf.ff" 'Terminus (TTF)' "$TERMINUS_DIR"/ter-u*`expr substr "$weight" 1 1`.bdf
-	popd 1>/dev/null
+	cd "$weight"
+	fontforge -lang=ff -script "${MYDIR}/mkttf.ff" 'Terminus (TTF)' \
+		"$TERMINUS_DIR"/ter-u*`echo "$weight"|cut -b 1`.bdf
+	cd - 1>/dev/null
 
 	echo
 done
